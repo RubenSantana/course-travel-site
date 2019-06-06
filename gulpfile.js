dfe33a534b9d039ea62c5d80f21754ae5ac38af4
RubenSantana <ruben_santana1990@hotmail.com>
2019-06-06 23:07:28 +0100

    Playing with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const watch = require("gulp-watch");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssvars = require("postcss-simple-vars");
+const nested = require("postcss-nested");
+
+gulp.task("default", function() {
+    console.log("Hooray - you created a Gulp task");
+});
+
+gulp.task("html", function () {
+    console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task("styles", function () {
+    return gulp.src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
+});
+
+gulp.task("watch", function () {
+
+    watch("./app/index.html", function () {
+        gulp.start("html");
+    });
+
+    watch("./app//assets/styles/**/*.css", function () {
+        gulp.start("styles");
+    });
+
+});
+
